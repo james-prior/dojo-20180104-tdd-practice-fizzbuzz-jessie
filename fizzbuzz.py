e3018080cadfd784da8eadf387f52b15efb07a63
James Prior and Jessie <jep200404@columbus.rr.com>
2018-01-05 12:31:46 -0500

    fizzbuzz.py: Refactor: Delete trailing whitespace.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz(doodle):
     output = ''
-    if doodle % 3 == 0: 
+    if doodle % 3 == 0:
         output += 'Fizz'
-    if doodle % 5 == 0: 
+    if doodle % 5 == 0:
         output += 'Buzz'
     if not output:
         output = str(doodle)
