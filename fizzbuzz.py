e5de1be40e60da909426514679f2ae2692269a7f
James Prior and Jessie <jep200404@columbus.rr.com>
2018-01-04 20:41:40 -0500

    fizzbuzz.py: Refactored for input that is not multiple of 3 or 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
 def fizzbuzz(doodle):
-    if doodle == 1: 
-        return '1'
-    elif doodle == 2: 
-        return '2'
-    elif doodle % 3 == 0 and doodle % 5 == 0:
+    if doodle % 3 == 0 and doodle % 5 == 0:
         return 'FizzBuzz'
     elif doodle % 5 == 0:
         return 'Buzz'
