6a44bb7214715c6a6eb7267a37a0a1b64397ce37
James Prior and Jessie <jep200404@columbus.rr.com>
2018-01-04 19:58:27 -0500

    fizzbuzz.py: Wrote code to pass test.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz(doodle):
         return '1'
     elif doodle == 2: 
         return '2'
-    elif doodle in (6, 3, 9): 
+    elif doodle in (6, 3, 9, 12): 
         return 'Fizz'
     else:
         return 'Buzz'
