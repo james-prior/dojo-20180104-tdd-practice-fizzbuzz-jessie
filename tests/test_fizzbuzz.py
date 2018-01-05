26ba5eae7d6732ec4d0abe009496e44abd568b32
James Prior and Jessie <jep200404@columbus.rr.com>
2018-01-04 19:46:44 -0500

    tests/test_fizzbuzz.py: Wrote failing test for 5 -> 'Buzz'.

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -6,6 +6,7 @@ number_to_expected_string = {
     1: '1',
     2: '2',
     3: 'Fizz',
+    5: 'Buzz',
 }
 @pytest.mark.parametrize('number, expected_string', number_to_expected_string.items())
 def test_known_number_returns_expected(number, expected_string):
