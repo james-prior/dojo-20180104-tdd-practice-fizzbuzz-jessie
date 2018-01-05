9b85bd3cb1089f5cc5dc3dc3f1bf682b357e9c3d
James Prior and Jessie <jep200404@columbus.rr.com>
2018-01-04 20:29:51 -0500

    fizzbuzz.py: Refactored detection of multiples for FizzBuzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz(doodle):
         return '2'
     elif doodle in (5, 10):
         return 'Buzz'
-    elif doodle == 15:
+    elif doodle % 5 == 0:
         return 'FizzBuzz'
     elif doodle % 3 == 0: 
         return 'Fizz'
